7fbf33a7beb695d2a6d4aac73211c1883576bb40
itl95pps <itl95pps@gmail.com>
2020-04-15 18:04:37 -0300

    Upload project

---
+++ functions/f/scheduleMessages.js
@@ -0,0 +1,63 @@
+const firebase = require('firebase/app');
+const moment = require('moment');
+const axios = require('axios');
+const { uniqBy, delay } = require('lodash');
+
+const scheduleMessages = async function() {
+  try {
+    const notificationsRef = firebase.firestore().collection('notification');
+    const snapshot = await notificationsRef
+      .where('sended', '==', false)
+      .where('hour', '==', moment().subtract(3, 'h').format('HH:mm')) // para o firebase 
+      .get();
+
+    const messages = [];
+    if (!snapshot.empty) {
+      snapshot.forEach(doc => {
+        messages.push({
+          ...doc.data(),
+          uid: doc.id
+        });
+      });
+    }
+
+    const organizationsId = 
+      uniqBy(messages, 'organizationId')
+      .map(x => (x.organizationId));
+    
+    const snapshotSlack = await firebase.firestore().collection('slack').get();
+    if (!snapshotSlack.empty) {
+      snapshotSlack.forEach(doc => {
+        const message = messages.filter(x => x.organizationId == doc.id)
+        for (let index = 0; index < message.length; index++) {
+          const m = message[index];
+          m.slackBotToken = doc.data().botToken;
+        }
+      });
+    }
+
+    for (let index = 0; index < messages.length; index++) {
+      const message = messages[index];
+      delay((m) => {
+        for (let toIndex = 0; toIndex < m.to.length; toIndex++) {
+          const to = m.to[toIndex];
+          axios.get(
+            'https://slack.com/api/chat.postMessage?token=' + m.slackBotToken +
+            '&channel=' + to +
+            '&text=' + m.message
+          );
+        }
+      }, 100, message);
+
+      firebase
+        .firestore()
+        .collection('notification')
+        .doc(message.uid)
+        .update({ sended: true });
+    }
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+module.exports = scheduleMessages;
---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const functions = require('firebase-functions');
+const { auth } = require('firebase');
+const firebase = require('firebase/app');
+require('firebase/firestore');
+
+const env = require('./.env.json');
+
+const scheduleMessages = require('./f/scheduleMessages');
+
+const authFirebase = async () => {
+  const { userFirebase, passwordFirebase } = env;
+  await auth().signInWithEmailAndPassword(
+    userFirebase,
+    passwordFirebase
+  );
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(env.firebaseConfig);
+}
+
+/* const teste = async (req, res) => {
+  await authFirebase();
+  scheduleMessages();
+  res.send('ok')
+} */
+
+const sendMessage = async (req, res) => {
+  try {
+    await authFirebase();
+    scheduleMessages();
+    console.log(Date())
+  } catch (error) {
+    console.log('sendMessage -> error', error)
+  }
+} 
+
+/* exports.teste = functions.https.onRequest(async (req, res) => teste(req, res)); */
+exports.job = functions.pubsub.schedule('0 * * * *').onRun(context => sendMessage(req, res));
+
+console.log('---- build ----');
